b1ca48ae90a566c36ef841d3a6beb1699c4b2da7
WojtekSasiela <wojtek.sasiela@gmail.com>
2015-03-03 16:26:55 +0100

    usuniecie wykresu z sredniaavtivity

--- src/WojtekSasiela/DziennikSzkolny/SredniaAcitivity.java
+++ src/WojtekSasiela/DziennikSzkolny/SredniaAcitivity.java
@@ -26,158 +26,19 @@ import org.achartengine.renderer.XYSeriesRenderer;
  */
 public class SredniaAcitivity extends Activity {
 
-    private GraphicalView mChart;
-
-    private String[] mMonth = new String[] {
-            "Jan", "Feb" , "Mar", "Apr", "May", "Jun",
-            "Jul", "Aug" , "Sep", "Oct", "Nov", "Dec"
-    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.srednia_layout);
 
-        // Pokaz_Activity_z_klasy(R.id.otworz_srednia_button, getApplicationContext(),SredniaAcitivity.class);
-        //rysujWykres_Srednia();
-        openChart();
 
-        // Laczy operacje zamkniecia z konkrentym buttonem
         zamknijOkno(R.id.zamknij_srednia);
     }
 
-    public void rysujWykres_Srednia()
-    {
-        // init example series data
-        GraphViewSeries exampleSeries = new GraphViewSeries(new GraphView.GraphViewData[] {
-                new GraphView.GraphViewData(1, 3.0d)
-                , new GraphView.GraphViewData(2, 3.5d)
-                , new GraphView.GraphViewData(3, 3.5d)
-                , new GraphView.GraphViewData(4, 3.0d)
-        });
 
-        GraphView graphView = new LineGraphView(
-                this // context
-                , "Srednia arytmetyczna" // heading
-        );
-
-        graphView.addSeries(exampleSeries); // data
-
-        LinearLayout layout = (LinearLayout) findViewById(R.id.chart_container);
-        layout.addView(graphView);
-
-    }
-
-
-    private void openChart(){
-        int[] x = { 0,1,2,3,4,5,6,7 };
-//        int[] income = { 2000,2500,2700,3000,2800,3500,3700,3800};
-        int[] income = { 4, 4, 4, 4, 4, 4, 4, 4 };
-        int[] expense = {3, 4, 2, 5, 3, 1, 6, 3 };
-
-        // Creating an  XYSeries for Income
-        XYSeries incomeSeries = new XYSeries("Srednia");
-        // Creating an  XYSeries for Expense
-        XYSeries expenseSeries = new XYSeries("Oceny");
-        // Adding data to Income and Expense Series
-        for(int i=0;i<x.length;i++){
-            incomeSeries.add(x[i], income[i]);
-            expenseSeries.add(x[i],expense[i]);
-        }
-
-        // Creating a dataset to hold each series
-        XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
-        // Adding Income Series to the dataset
-        dataset.addSeries(incomeSeries);
-        // Adding Expense Series to dataset
-        dataset.addSeries(expenseSeries);
-
-
-        // Creating XYSeriesRenderer to customize incomeSeries
-        XYSeriesRenderer incomeRenderer = new XYSeriesRenderer();
-        incomeRenderer.setColor(Color.WHITE);
-        incomeRenderer.setPointStyle(PointStyle.CIRCLE);
-        incomeRenderer.setFillPoints(true);
-        incomeRenderer.setLineWidth(2);
-        incomeRenderer.setDisplayChartValues(true);
-
-
-        // Creating XYSeriesRenderer to customize expenseSeries
-        XYSeriesRenderer expenseRenderer = new XYSeriesRenderer();
-        expenseRenderer.setColor(Color.YELLOW);
-        expenseRenderer.setPointStyle(PointStyle.CIRCLE);
-        expenseRenderer.setFillPoints(true);
-        expenseRenderer.setLineWidth(2);
-        expenseRenderer.setDisplayChartValues(true);
-
-
-
-        // Creating a XYMultipleSeriesRenderer to customize the whole chart
-        XYMultipleSeriesRenderer multiRenderer = new XYMultipleSeriesRenderer();
-        multiRenderer.setXLabels(0);
-        multiRenderer.setChartTitle("Income vs Expense Chart");
-        multiRenderer.setXTitle("Year 2012");
-        multiRenderer.setYTitle("Amount in Dollars");
-        multiRenderer.setZoomButtonsVisible(true);
-        multiRenderer.setBarSpacing(4);
-        for(int i=0;i<x.length;i++){
-            multiRenderer.addXTextLabel(i, mMonth[i]);
-        }
-
-        // Adding incomeRenderer and expenseRenderer to multipleRenderer
-        // Note: The order of adding dataseries to dataset and renderers to multipleRenderer
-        // should be same
-        multiRenderer.addSeriesRenderer(incomeRenderer);
-        multiRenderer.addSeriesRenderer(expenseRenderer);
-
-        // Getting a reference to LinearLayout of the MainActivity Layout
-        LinearLayout chartContainer = (LinearLayout) findViewById(R.id.chart_container);
-
-
-        // Specifying chart types to be drawn in the graph
-        // Number of data series and number of types should be same
-        // Order of data series and chart type will be same
-        String[] types = new String[] { LineChart.TYPE, BarChart.TYPE };
-
-        // Creating a combined chart with the chart types specified in types array
-        mChart = (GraphicalView) ChartFactory.getCombinedXYChartView(getBaseContext(), dataset, multiRenderer, types);
-
-        multiRenderer.setClickEnabled(true);
-        multiRenderer.setSelectableBuffer(10);
-        mChart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                SeriesSelection seriesSelection = mChart.getCurrentSeriesAndPoint();
-
-                if (seriesSelection != null) {
-                    int seriesIndex = seriesSelection.getSeriesIndex();
-                    String selectedSeries="Income";
-                    if(seriesIndex==0)
-                        selectedSeries = "Income";
-                    else
-                        selectedSeries = "Expense";
-                    // Getting the clicked Month
-                    String month = mMonth[(int)seriesSelection.getXValue()];
-                    // Getting the y value
-                    int amount = (int) seriesSelection.getValue();
-                    Toast.makeText(
-                            getBaseContext(),
-                            selectedSeries + " in " + month + " : " + amount,
-                            Toast.LENGTH_SHORT).show();
-                }
-            }
-
-        });
-
-        // Adding the Combined Chart to the LinearLayout
-        chartContainer.addView(mChart);
-    }
-
-
-    public void zamknijOkno(int id)
-    {
-        Button b = (Button)findViewById(id);
+    public void zamknijOkno(int id) {
+        Button b = (Button) findViewById(id);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
