be7db161b960d1db248f392dc1a2bad7810222a7
WojtekSasiela <wojtek.sasiela@gmail.com>
2015-03-04 19:24:12 +0100

    wysuwajace sie dane w ListView po kliknieciu na (jego) elementy

--- src/WojtekSasiela/DziennikSzkolny/ListaKlasActivity.java
+++ src/WojtekSasiela/DziennikSzkolny/ListaKlasActivity.java
@@ -36,38 +36,14 @@ public class ListaKlasActivity extends Activity {
         listaKompoment2 = (ListView) findViewById(R.id.listView0);
         klasa1 = (Button) findViewById(R.id.klasa1);
 
-        // Connect with Database ORM
-        DatabaseHelper dbHelper = OpenHelperManager.getHelper(this, DatabaseHelper.class);
-        RuntimeExceptionDao<Student, Integer> studentDao = dbHelper.getStudentRuntimeExceptionDao();
-        List<Student> students = studentDao.queryForAll();
-        String cars[] =
-                {
-                        students.get(0).getName()  +  " " + students.get(0).getSurname() ,
-                        students.get(1).getName()  +  " " + students.get(1).getSurname() ,
-                        students.get(2).getName()  +  " " + students.get(2).getSurname() ,
-                        students.get(3).getName()  +  " " + students.get(3).getSurname() ,
-                        students.get(4).getName()  +  " " + students.get(4).getSurname() ,
-                        students.get(5).getName()  +  " " + students.get(5).getSurname() ,
-                        students.get(6).getName()  +  " " + students.get(6).getSurname() ,
-                        students.get(7).getName()  +  " " + students.get(7).getSurname() ,
-                        students.get(8).getName()  +  " " + students.get(8).getSurname() ,
-                        students.get(9).getName()  +  " " + students.get(9).getSurname() ,
-                };
-        //String cars[] = {"Ania Kowalska", "Joasia Pyrzyńska", "Izabela Tarnowska", "Blanka Szept", "Paweł Paluch", "Piotrek Mały", "Karol Kopytko", "Arkadiusz Bąk", "Teresa Wawrzyniak"};
-        String subjects[] = {"Polski", "Angielski", "Matematyka", "Przyroda", "Religia", "WF"};
-        ArrayList<String> carL = new ArrayList<String>();
-        ArrayList<String> subjectsL = new ArrayList<String>();
-        carL.addAll( Arrays.asList(cars) );
-        subjectsL.addAll( Arrays.asList(subjects) );
-        adapter = new ArrayAdapter<String>(this, R.layout.elementy_listy_glownej, carL);
-        adapterSubcjets = new ArrayAdapter<String>(this, R.layout.elementy_listy_glownej, subjectsL );
-        listaKompoment.setAdapter(adapter);
-        listaKompoment2.setAdapter(adapterSubcjets);
 
         klasa1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 view.setBackgroundColor(Color.BLUE);
+
+                pokazListeOsob();
+
             }
         });
 
@@ -79,9 +55,18 @@ public class ListaKlasActivity extends Activity {
 
                 // change the background color of the selected element
                 view.setBackgroundColor(Color.BLUE);
+
+
+                String subjects[] = {"Polski", "Angielski", "Matematyka", "Przyroda", "Religia", "WF"};
+                ArrayList<String> subjectsL = new ArrayList<String>();
+                subjectsL.addAll(Arrays.asList(subjects));
+                adapterSubcjets = new ArrayAdapter<String>(view.getContext(), R.layout.elementy_listy_glownej, subjectsL);
+                listaKompoment2.setAdapter(adapterSubcjets);
             }
         });
 
+
+        //interakcja na klikniecie na nazwe przedmioty z listy przedmiotow
         listaKompoment2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -91,26 +76,49 @@ public class ListaKlasActivity extends Activity {
                 // change the background color of the selected element
                 view.setBackgroundColor(Color.BLUE);
 
-                        Intent intent = new Intent(getApplicationContext(), DaneUczniaActivity.class);
+                Intent intent = new Intent(getApplicationContext(), DaneUczniaActivity.class);
 
-                        if (intent.resolveActivity(getPackageManager()) != null)
-                            startActivity(intent);
-                        else {
-                            Toast.makeText(getApplicationContext(), "Niestety, ale startActivityForResult wywala blad.",
-                                    Toast.LENGTH_LONG).show();
-                        }
+                if (intent.resolveActivity(getPackageManager()) != null)
+                    startActivity(intent);
+                else {
+                    Toast.makeText(getApplicationContext(), "Niestety, ale startActivityForResult wywala blad.",
+                            Toast.LENGTH_LONG).show();
+                }
 
 
             }
         });
         zamknijOkno(R.id.Wyjdz_button);
+   }
 
-        Toast.makeText(getApplicationContext(), "Pokaz liste klas", Toast.LENGTH_LONG).show();
+    public void pokazListeOsob() {
+        // Connect with Database ORM
+        DatabaseHelper dbHelper = OpenHelperManager.getHelper(this, DatabaseHelper.class);
+        RuntimeExceptionDao<Student, Integer> studentDao = dbHelper.getStudentRuntimeExceptionDao();
+        List<Student> students = studentDao.queryForAll();
+        String cars[] =
+                {
+                        students.get(0).getName() + " " + students.get(0).getSurname(),
+                        students.get(1).getName() + " " + students.get(1).getSurname(),
+                        students.get(2).getName() + " " + students.get(2).getSurname(),
+                        students.get(3).getName() + " " + students.get(3).getSurname(),
+                        students.get(4).getName() + " " + students.get(4).getSurname(),
+                        students.get(5).getName() + " " + students.get(5).getSurname(),
+                        students.get(6).getName() + " " + students.get(6).getSurname(),
+                        students.get(7).getName() + " " + students.get(7).getSurname(),
+                        students.get(8).getName() + " " + students.get(8).getSurname(),
+                        students.get(9).getName() + " " + students.get(9).getSurname(),
+                };
+        //String cars[] = {"Ania Kowalska", "Joasia Pyrzyńska", "Izabela Tarnowska", "Blanka Szept", "Paweł Paluch", "Piotrek Mały", "Karol Kopytko", "Arkadiusz Bąk", "Teresa Wawrzyniak"};
+        ArrayList<String> carL = new ArrayList<String>();
+        carL.addAll(Arrays.asList(cars));
+        adapter = new ArrayAdapter<String>(this, R.layout.elementy_listy_glownej, carL);
+        listaKompoment.setAdapter(adapter);
+
     }
 
-    public void zamknijOkno(int id)
-    {
-        Button b = (Button)findViewById(id);
+    public void zamknijOkno(int id) {
+        Button b = (Button) findViewById(id);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -118,7 +126,6 @@ public class ListaKlasActivity extends Activity {
             }
         });
     }
-
 
 
 }
