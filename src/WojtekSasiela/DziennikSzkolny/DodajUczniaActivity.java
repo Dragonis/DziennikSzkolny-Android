d0218f263c49c88e05b90e826a45e76668c4ddbe
WojtekSasiela <wojtek.sasiela@gmail.com>
2015-03-10 15:16:56 +0100

    savepoint

--- src/WojtekSasiela/DziennikSzkolny/DodajUczniaActivity.java
+++ src/WojtekSasiela/DziennikSzkolny/DodajUczniaActivity.java
@@ -3,7 +3,10 @@ package WojtekSasiela.DziennikSzkolny;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -21,7 +24,20 @@ public class DodajUczniaActivity extends Activity {
             public void onClick(View view) {
                 //TODO DOdanie uzytkownika do bazy danych
 
-                Toast.makeText(getApplicationContext(), "Użytkownik został zapisany",
+                EditText imie_edittext = (EditText) findViewById(R.id.imie_edittext_dodajucznia);
+                EditText nazwisko_edittext = (EditText) findViewById(R.id.nazwisko_edittext_dodajucznia);
+                EditText klasa_edittext = (EditText) findViewById(R.id.klasa_edittext_dodajucznia);
+
+                String imie = imie_edittext.getText().toString();
+                String nazwisko = nazwisko_edittext.getText().toString();
+                String klasa = klasa_edittext.getText().toString();
+
+                przeslijDaneDoWczesniejszegoActivity(imie, nazwisko, klasa);
+//
+//                Toast.makeText(getApplicationContext(), "Użytkownik został zapisany",
+//                        Toast.LENGTH_SHORT).show();
+
+                Toast.makeText(getApplicationContext(), "Imie: "+ imie + "Naziwsko: "+ nazwisko + "Klasa: "+ klasa,
                         Toast.LENGTH_SHORT).show();
             finish();
             }
@@ -38,6 +54,10 @@ public class DodajUczniaActivity extends Activity {
 //            }
 //        });
 }
+    public void przeslijDaneDoWczesniejszegoActivity(String imie, String nazwisko, String klasa)
+    {
+
+    }
 
     public void zamknijOkno(int id)
     {
