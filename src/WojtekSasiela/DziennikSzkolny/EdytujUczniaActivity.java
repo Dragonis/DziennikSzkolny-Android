31bb76fce98d22913fc03c8907687ba8f39e3736
WojtekSasiela <wojtek.sasiela@gmail.com>
2015-03-10 15:37:53 +0100

    przekazywanie zmiennych z edytujbutton do listaklasActoivity (wczesniejszy komit robil to samo tylko ze z dodajbutton)

--- src/WojtekSasiela/DziennikSzkolny/EdytujUczniaActivity.java
+++ src/WojtekSasiela/DziennikSzkolny/EdytujUczniaActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 /**
@@ -16,11 +17,29 @@ public class EdytujUczniaActivity extends Activity {
         setContentView(R.layout.edytujucznia_layout);
 
         Button zapisz = (Button) findViewById(R.id.zapisz_button_edytujucznia);
+
+
+
         zapisz.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Dany zostały zmienione",
+//                Toast.makeText(getApplicationContext(), "Dany zostały zmienione",
+//                        Toast.LENGTH_SHORT).show();
+                EditText id_edittext = (EditText) findViewById(R.id.id_edittext_edytujucznia);
+                EditText imie_edittext = (EditText) findViewById(R.id.imie_edittext_edytujucznia);
+                EditText nazwisko_edittext = (EditText) findViewById(R.id.nazwisko_edittext_edytujucznia);
+                EditText klasa_edittext = (EditText) findViewById(R.id.klasa_edittext_edytujucznia);
+
+                String id = imie_edittext.getText().toString();
+                String imie = imie_edittext.getText().toString();
+                String nazwisko = nazwisko_edittext.getText().toString();
+                String klasa = klasa_edittext.getText().toString();
+
+                przeslijDaneDoWczesniejszegoActivity(imie,nazwisko,klasa);
+
+                Toast.makeText(getApplicationContext(), "ID: "+ id + " ZMIENIONO NA: Imie: "+ imie + " Naziwsko: "+ nazwisko + " Klasa: "+ klasa,
                         Toast.LENGTH_SHORT).show();
+
                 finish();
             }
         });
@@ -36,6 +55,12 @@ public class EdytujUczniaActivity extends Activity {
 //            }
 //        });
     }
+
+    public void przeslijDaneDoWczesniejszegoActivity(String imie, String nazwisko, String klasa)
+    {
+
+    }
+
     public void zamknijOkno(int id)
     {
         Button b = (Button)findViewById(id);
