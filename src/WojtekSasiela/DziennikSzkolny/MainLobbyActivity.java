203309655b076c9f07375cab4a3af71e51c4a5e5
WojtekSasiela <wojtek.sasiela@gmail.com>
2015-07-26 18:36:52 +0200

    usuniecie dawne wyloguj_button a zrobienie zamknij_button na wyloguj_button

--- src/WojtekSasiela/DziennikSzkolny/MainLobbyActivity.java
+++ src/WojtekSasiela/DziennikSzkolny/MainLobbyActivity.java
@@ -1,7 +1,5 @@
 package WojtekSasiela.DziennikSzkolny;
 
-import WojtekSasiela.DziennikSzkolny.ORM.configuration.DatabaseAccessObjects;
-import WojtekSasiela.DziennikSzkolny.ORM.CRUD.DatabaseCRUDoperations;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -10,9 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.j256.ormlite.android.apptools.OpenHelperManager;
-
-import java.sql.SQLException;
 
 public class MainLobbyActivity extends Activity {
     /**
@@ -20,7 +15,7 @@ public class MainLobbyActivity extends Activity {
      */
     public static String TAG = "DziennikSzkolny";
 
-    Button zamknij_aplikacje;
+    Button wyloguj;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,14 +23,13 @@ public class MainLobbyActivity extends Activity {
         setContentView(R.layout.main);
         odbierzDanezPoprzedniegoActivity_iWyswietl();
 
-        WyswietlButton_i_PrzelaczNaActivityzKlasy(R.id.Wyloguj_button, getApplicationContext(), LogowanieActivity.class);
         WyswietlButton_i_PrzelaczNaActivityzKlasy(R.id.OpcjeButton, getApplicationContext(), OpcjeAcitivity.class);
         WyswietlButton_i_PrzelaczNaActivityzKlasy(R.id.UczniowieButton, getApplicationContext(), OAutorzeActivity.class);
         WyswietlButton_i_PrzelaczNaActivityzKlasy(R.id.ZobaczStatyButton, getApplicationContext(), StatystykiActivity.class);
         WyswietlButton_i_PrzelaczNaActivityzKlasy(R.id.ListaKlas_Button, getApplicationContext(), ListaKlasActivity.class);
 
-        zamknij_aplikacje = (Button) findViewById(R.id.zamknijaplikacje_button);
-        zamknij_aplikacje.setOnClickListener(new View.OnClickListener() {
+        wyloguj = (Button) findViewById(R.id.wyloguj_button);
+        wyloguj.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
