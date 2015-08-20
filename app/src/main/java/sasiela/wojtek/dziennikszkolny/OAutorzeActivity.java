da0ede6e8c37a29fd979b4a52bea8751dd2e7853
WojtekSasiela <wojtek.sasiela@gmail.com>
2015-08-20 18:17:37 +0200

    O Autorze - przeniesienie tekstu do strings.xml

--- app/src/main/java/sasiela/wojtek/dziennikszkolny/OAutorzeActivity.java
+++ app/src/main/java/sasiela/wojtek/dziennikszkolny/OAutorzeActivity.java
@@ -17,11 +17,32 @@ import java.util.HashMap;
 public class OAutorzeActivity extends Activity {
 
 
+    private TextView autor_textview;
+    private TextView tematpracy_textview;
+    private TextView promotor_textview;
+    private TextView uczelnia_textview;
+    private TextView wydzial_textview;
+    private TextView kierunek_textview;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.oautorze_layout);
         zamknijOkno(R.id.Zamknij_Uczniowie);
+
+        autor_textview = (TextView) findViewById(R.id.autor_textview);
+        tematpracy_textview = (TextView) findViewById(R.id.tematpracy_textview);
+        promotor_textview = (TextView) findViewById(R.id.promotor_textview);
+        uczelnia_textview = (TextView) findViewById(R.id.uczelnia_textview);
+        wydzial_textview = (TextView) findViewById(R.id.wydzial_textview);
+        kierunek_textview = (TextView) findViewById(R.id.kierunek_textview);
+
+        autor_textview.setText(R.string.AutorProgramu);
+        tematpracy_textview.setText(R.string.TematPracyMagisterskiej);
+        promotor_textview.setText(R.string.Promitor);
+        uczelnia_textview.setText(R.string.Uczelnia);
+        wydzial_textview.setText(R.string.Wydział);
+        kierunek_textview.setText(R.string.Kierunek);
 
     }
 
